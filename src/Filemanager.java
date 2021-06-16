11e340ac2bbcd3c9e3009dccc4d5ec3bd02858f9
shadow0o0 <37043722+shadow0o0@users.noreply.github.com>
2021-06-16 14:29:55 +0300

    Initial commit

---
+++ src/Filemanager.java
@@ -0,0 +1,13 @@
+
+import screens.WelcomeScreen;
+
+public class Filemanager {
+
+    public static void main(String[] args) {
+    	
+    	WelcomeScreen welcome = new WelcomeScreen();
+    	welcome.introWS();
+    	welcome.GetUserInput();
+
+    }
+}
